eb08b2c7c9a5ebe21788f513c715e6d7412270e8
Diti2604 <ditiboyboy@gmail.com>
2023-11-17 11:38:28 +0100

    complete the search header options

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -11,21 +11,28 @@ export default function SearchBox() {
   const router = useRouter();
   const [term, setTerm] = useState(searchTerm || "");
 
-  function handleSubmit(e){
- e.preventDefault();
- if(!term.trim()) return;
-router.push(`/search/web?searchTerm=${term}`)
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!term.trim()) return;
+    router.push(`/search/web?searchTerm=${term}`);
   }
   return (
     <form
       className="flex border border-gray-200 rounded-full
      shadow-lg px-6 py-3 ml-10 mr-5 flex-grow max-w-3xl items-center
     "
-    onSubmit={handleSubmit}
+      onSubmit={handleSubmit}
     >
-      <input type="text" className="w-full focus:outline-none" value={term} onChange={(e) => setTerm(e.target.value)}
+      <input
+        type="text"
+        className="w-full focus:outline-none"
+        value={term}
+        onChange={(e) => setTerm(e.target.value)}
       />
-      <RxCross2 className="text-2xl text-gray-500 cursor-pointer sm:mr-2" onClick={() => setTerm("")}/>
+      <RxCross2
+        className="text-2xl text-gray-500 cursor-pointer sm:mr-2"
+        onClick={() => setTerm("")}
+      />
       <BsFillMicFill
         className="hidden sm:inline-flex text-4xl
        text-blue-500 pl-4 border-l-2 border-gray-300
@@ -34,7 +41,7 @@ router.push(`/search/web?searchTerm=${term}`)
       <AiOutlineSearch
         className="text-2xl hidden sm:inline-flex text-blue-500 cursor-pointer
       "
-      onClick={handleSubmit}
+        onClick={handleSubmit}
       />
     </form>
   );
